c52174bade618d855d64542b4107dfc25fb1e775
eldom93 <55229671+eldom93@users.noreply.github.com>
2020-01-22 13:39:09 -0600

    bound data to component

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 mongoose.connect('mongodb+srv://neldo:ILoveAllah1!@cluster0-eminl.mongodb.net/test?retryWrites=true&w=majority');
 mongoose.connection.once('open',() => {
